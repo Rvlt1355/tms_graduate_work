8026123b2052fdc193206d99c05c5ae721914ba4
Rvlt1355 <84668209+Rvlt1355@users.noreply.github.com>
2021-09-29 21:51:06 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="chromedriver")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(4)
     yield driver
