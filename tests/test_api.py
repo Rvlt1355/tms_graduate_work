ef032d118cab55b8081ef01b0cd5d93f3b0a6eac
igor.vanin <igor.vanin@shtormtech.ru>
2021-09-29 22:44:23 +0300

    change test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,16 @@
 import allure
 from framework.api_helpers.expected_results import CollectionExpectedResult as ER
-from time import sleep
 
 
 @allure.feature('Тест API')
 def test_api_create_user(api_client):
-    with allure.step('Создаем пользователя'):
+    with allure.step('Create user'):
         api_client.func_create_user(ER.CREATE_USER_RESPONSE)
-        sleep(3)
-    with allure.step('Логинимся тестовым пользователем'):
+    with allure.step('Login test user'):
         api_client.func_login_user()
-    with allure.step('Получаем данные пользователя'):
+    with allure.step('Get info test user'):
         api_client.func_get_user_info(ER.GET_USER_INFO_RESPONSE)
-    with allure.step('Логаут пользователем'):
+    with allure.step('Logout'):
         api_client.func_logout_user(ER.LOGOUT_RESPONSE)
-    with allure.step('Удаляем пользоваетеля'):
+    with allure.step('Delete user'):
         api_client.delete_user()
