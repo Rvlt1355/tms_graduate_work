ad9752bbd5dfd3f154991229015bb7836aa1932c
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 01:27:20 +0300

    fix chrome docker

--- conftest.py
+++ conftest.py
@@ -11,10 +11,9 @@ def driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
-    # driver = webdriver.Chrome()
-    # driver.maximize_window()
     driver.implicitly_wait(4)
     yield driver
     driver.quit()
