2f67cbaca718362357d063882f4f39ba813d40b7
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 01:54:12 +0300

    fix chrome docker

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ def driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument('--whitelisted-ips="127.0.0.1"')
     driver = webdriver.Chrome(options=chrome_options)
