75cee15d30efe8ee7c8edc3901457eb7cbe9adbf
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-02 18:43:46 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ from framework.api_helpers.api_functionality import FuncApi
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     # driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
