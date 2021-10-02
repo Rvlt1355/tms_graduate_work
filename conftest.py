b946462b549cf25b3b7624c0b7dc45f1853043b2
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 01:58:56 +0300

    fix chrome docker

--- conftest.py
+++ conftest.py
@@ -11,10 +11,9 @@ def driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--whitelisted-ips="127.0.0.1"')
-    chrome_options.add_argument('--port=4444')
+    #chrome_options.add_argument('--whitelisted-ips="127.0.0.1"')
+    #chrome_options.add_argument('--port=4444')
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument('--whitelisted-ips="127.0.0.1"')
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(4)
     yield driver
--- framework/pages/__init__.py
+++ framework/pages/__init__.py
@@ -6,5 +6,7 @@ from framework.pages.admin_page import AdminPage
 class UIWorker(UserPage, LoginPage, AdminPage):
     """Класс через наследование работает со всеми экранами"""
     """Метод для открытия нужного экрана"""
-    def open_screen(self, url='http://localhost:8000/'):
+    def open_screen(self, url='https://translate.google.com/?hl=ru'):
         self.open_page(url)
+    """def open_screen(self, url='http://localhost:8000/'):
+        self.open_page(url)"""
