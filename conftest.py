4bf7763f64f30e39be595cf8f38fa6c0f65dc6cd
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 11:44:54 +0300

    fix chrome docker

--- conftest.py
+++ conftest.py
@@ -9,9 +9,9 @@ from framework.api_helpers.api_functionality import FuncApi
 @pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(4)
