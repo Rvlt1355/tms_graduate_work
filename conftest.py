dac35fd9fe49c0ca944ce5f0cdc01650b580d57b
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 11:10:36 +0300

    fix chrome docker

--- conftest.py
+++ conftest.py
@@ -2,18 +2,18 @@ import pytest
 from selenium import webdriver
 from framework.db_client import ClientDB
 from framework.pages import UIWorker
+
 from framework.api_helpers.api_functionality import FuncApi
 
 
 @pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--whitelisted-ips="127.0.0.1"')
+    chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(4)
     yield driver
     driver.quit()
