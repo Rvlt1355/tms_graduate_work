ca6af97e0939b0c17c385283ea427ae57f59164d
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-03 12:01:48 +0300

    change conftest and delete test_file

--- conftest.py
+++ conftest.py
@@ -13,7 +13,6 @@ def driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--window-size=1920,1080")
-    # driver = webdriver.Chrome(options=chrome_options)
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(4)
     yield driver
--- tests/test_file.py
+++
@@ -1,6 +0,0 @@
-from framework.locators import locators as lc
-from selenium.webdriver.common.by import By
-
-
-def test_google(pages):
-    pages.find_element(By.CSS_SELECTOR('[alt="Google"]'))
