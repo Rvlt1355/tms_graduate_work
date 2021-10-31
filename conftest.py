5ad37f09dae51868ae61669ed8a89957b3cb6c10
igor.vanin <igor.vanin@shtormtech.ru>
2021-10-31 19:57:28 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -8,14 +8,16 @@ from framework.api_helpers.api_functionality import FuncApi
 
 @pytest.fixture
 def driver():
+    """
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
-    # driver = webdriver.Chrome()
-    # driver.maximize_window()
+    """
+    driver = webdriver.Chrome("chromedriver")
+    driver.maximize_window()
     driver.implicitly_wait(4)
     yield driver
     driver.quit()
